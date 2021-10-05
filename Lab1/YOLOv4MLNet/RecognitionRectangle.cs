e88af65a7450c280a0cff8cdbcef5e50d6edb2b3
u0da <soldatov2011@gmail.com>
2021-10-05 20:23:47 +0300

    task1

---
+++ Lab1/YOLOv4MLNet/RecognitionRectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace YOLOv4MLNet
+{
+   public class RecognitionRectangle
+    {
+        private readonly double x;
+        private readonly double y;
+        private readonly double height;
+        private readonly double width;
+        private readonly string label;
+
+        public RecognitionRectangle(double x, double y, double height, double width, string label)
+        {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+            this.width = width;
+            this.label = label;
+        }
+
+        public override string ToString()
+        {
+            return label + " [coordinates: (" + Math.Round(x,2).ToString() + " ; " + Math.Round(y, 2).ToString() + ") - height: "
+                + Math.Round(height, 2).ToString() + " - width: " + Math.Round(width, 2).ToString() + "]\n";
+        }
+    }
+}
